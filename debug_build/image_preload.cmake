65598575fe418d000ad86532aa22a595bcb8a72e
Rohan Lourens <rlourens@etse.co.za>
2024-04-16 16:31:34 +0200

    First commit

---
+++ debug_build/image_preload.cmake
@@ -0,0 +1,11 @@
+# Generated file that can be used to preload variant images
+set(BOARD "nrf52840dk_nrf52840" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "d:/Projects/Farmtrack/Sandbox/Rohan/Projects/ft_ble/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(CLI_CONFIG_CORTEX_M_DEBUG_MONITOR_HOOK "y" CACHE INTERNAL "NCS child image controlled")
+set(CLI_CONFIG_DEBUG_OPTIMIZATIONS "y" CACHE INTERNAL "NCS child image controlled")
+set(CLI_CONFIG_DEBUG_THREAD_INFO "y" CACHE INTERNAL "NCS child image controlled")
+set(CLI_CONFIG_SEGGER_DEBUGMON "y" CACHE INTERNAL "NCS child image controlled")
+set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "D:/Projects/Farmtrack/Sandbox/Rohan/Projects/ft_ble/nrf52840dk_nrf52840.overlay" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "C:/ncs/toolchains/cf2149caf2/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
--- src/main.c
+++ src/main.c
@@ -1,4 +1,36 @@
+/**
+ * @file main.c
+ * @author Rohan Lourens
+ * @brief 
+ * @version 0.1
+ * @date 2024-04-15
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+
+#include <soc.h>
+#include <stdio.h>
+#include <stddef.h>
 #include <zephyr/kernel.h>
+#include <zephyr/types.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/pm/pm.h>
+#include <zephyr/pm/device.h>
+#include <zephyr/pm/device_runtime.h>
+#include <zephyr/pm/state.h>
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/bluetooth/gatt.h>
+#include <zephyr/bluetooth/gap.h>
+#include <zephyr/bluetooth/hci.h>
+#include <zephyr/bluetooth/conn.h>
+
+
 
 int main(void)
 {
